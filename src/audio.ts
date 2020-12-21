e7847d19024c2668df5bd559de6fc65a72b0c47f
Peter Jaško <ptr.jasko@gmail.com>
2020-12-21 11:15:22 +0100

    for demo

--- src/audio.ts
+++ src/audio.ts
@@ -1,4 +1,5 @@
 import * as Tone from 'tone'
+import { StereoWidener } from 'tone'
 import { Scale } from './notes'
 
 
@@ -8,28 +9,58 @@ const notes = [9, 9, 9, ]
 // constants
 const distortion = 10
 
-export const distorter = new Tone.Distortion({distortion, oversample: '4x'}).toDestination()
-export const fm_synth = new Tone.FMSynth().connect(distorter)
-const synthB = new Tone.AMSynth().connect(distorter)
-
-const seq = new Tone.Sequence({
-  events: ['A1', 'A1', 'A1', 'E2', 'E2', 'A1', 'D2', 'G1'],
-  callback: (time, note) => { fm_synth.triggerAttackRelease(note, '32n', time) },
-  subdivision: '16n'
-})
+export const master_envelope = new Tone.Follower()
+export const meter = new Tone.Meter(.1)
+meter.toDestination()
+// Tone.Destination.connect(meter)
 
 const kick = new Tone.Player({
   // url: '../res/kshmr-kick-11.wav',
   loop: false,
   autostart: false
-}).toDestination()
+}).connect(meter)
 
 const kick_promise = kick.load('/kshmr-kick-11.wav')
 
+const kick_loop = new Tone.Loop((time) => {
+  console.log(kick.buffer)
+  kick.start(time)
+}, '4n')
+
+const note_pattern = [
+  'A1', 'A1', 'A1', 'E2', 'E2', 'A1', 'D2', 'G1',
+  'A1', 'A1', 'C2', 'C2', 'A1', 'A1', 'A1', 'A1',
+]
+const bass_note_pattern = [
+  'A0', 'A0', 'A0', 'E1', 'E1', 'A0', 'D1', 'G0',
+  'A0', 'A0', 'C1', 'C1', 'A0', 'A0', 'A0', 'A0',
+]
 
 // const kick_sample = new Tone.ToneAudioBuffer('../res/kshmr-kick-11.wav')
 
+export const fake_sidechain = new Tone.AmplitudeEnvelope(0.3, '0', 1, '0').toDestination()
+fake_sidechain.attackCurve = 'sine'
+const sidechain_loop = new Tone.Loop((time) => fake_sidechain.triggerAttackRelease('4n', time), '4n')
+// const widener = new StereoWidener(1).connect(fake_sidechain)
 
+export const distorter = new Tone.Distortion({distortion, oversample: '4x'}).connect(fake_sidechain)
+export const fm_synth = new Tone.FMSynth().connect(distorter)
+export const sub_bass = new Tone.Synth().connect(fake_sidechain)
+
+
+
+sub_bass.detune.value = 0
+const synthB = new Tone.AMSynth().connect(distorter)
+
+
+const seq = new Tone.Sequence({
+  events: note_pattern,
+  callback: (time, note) => {
+    sub_bass.triggerAttackRelease(note, '16n', time)
+    fm_synth.triggerAttackRelease(note, '32n', time)
+  },
+  subdivision: '16n'
+})
 
 // const drums = new Tone.Sampler({
 //   1: '../res/kshmr-kick-11.wav',
@@ -41,10 +72,6 @@ const kick_promise = kick.load('/kshmr-kick-11.wav')
 //   2: '../res/kshmr-snare-18.wav'
 // })
 
-const kick_loop = new Tone.Loop((time) => {
-  console.log(kick.buffer)
-  kick.start(time)
-}, '4n')
 
 
 // export const MOD = {
@@ -62,6 +89,7 @@ export const start_music = async () => {
   Tone.Transport.bpm.value = 95
   Tone.Transport.start()
   seq.start(0)
+  sidechain_loop.start(0)
   kick_loop.start(0)
 
   // the loops start when the Transport is started
--- src/sketch.ts
+++ src/sketch.ts
@@ -1,32 +1,64 @@
 import {Path, settings, view} from 'paper'
 import './audio'
-import { fm_synth } from './audio'
+import { fake_sidechain, fm_synth, master_envelope, meter, sub_bass } from './audio'
+import { p5_map } from './utils'
 
 export default () => {
-  settings.hitTolerance = 50
+  settings.hitTolerance = 200
   const path = new Path.RegularPolygon(view.center, 3, 100)
-  path.strokeColor = 'black' as any
-  path.fillColor = 'black' as any
+
+  // path.strokeColor = 'black' as any
+  path.fillColor = 'yellow' as any
   let dragged_curve: paper.Curve | undefined
+  let dragged_ix: number | undefined
+  const shadow_path = path.clone()
+  shadow_path.fillColor = undefined
+  shadow_path.strokeColor = 'yellow' as any
+
 
   path.onMouseDown = (event: any) => {
-    dragged_curve = path.curves.find(({point1}) => point1.isClose(event.point, 30))
+    path.fullySelected = true
+    dragged_ix = path.curves.findIndex(({point1}) => point1.isClose(event.point, 30))
+    if (dragged_ix != null) dragged_curve = path.curves[dragged_ix]
     // some_synth.triggerAttackRelease('C4', '8n')
   }
 
+  view.onFrame = () => {
+    shadow_path.copyContent(path)
+    shadow_path.fullySelected = false
+    // console.log(meter.getValue())
+    const meter_value = meter.getValue() as number
+    const by = meter_value < -20 ? 0 : (meter_value + 20) / 10
+    // console.log('scale', by)
+    shadow_path.scale(by)
+  }
+
   path.onMouseDrag = (event: any) => {
-    if (!dragged_curve) path.position = path.position.add(event.delta)
+    if (!dragged_curve) {
+      path.position = path.position.add(event.delta)
+      fm_synth.detune.value -= event.delta.y
+      sub_bass.detune.value -= event.delta.y
+    }
   }
 
   view.onMouseMove = (event: any) => {
     if (dragged_curve) {
       dragged_curve.point1 = event.point
-      fm_synth.modulationIndex.value = dragged_curve.point1.getDistance(view.center) / 5
-      console.log(fm_synth.modulationIndex.value)
+      const distance = dragged_curve.point1.getDistance(view.center)
+      switch (dragged_ix!) {
+        case 0:
+          fm_synth.modulationIndex.value = distance / 5
+          break
+        case 1:
+          fake_sidechain.attack = distance / 600
+      }
+
     }
   }
 
   path.onMouseUp = () => {
+    path.fullySelected = false
     dragged_curve = undefined
+    dragged_ix = undefined
   }
 }
